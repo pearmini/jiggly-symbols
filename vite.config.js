ca9258b47a9a44c0ac39b1dd1b9587aee614a41b
Bairui Su <subairui@icloud.com>
2025-03-30 23:35:45 -0400

    Single jiggly symbol (#1)

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+  // server: {
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
+  // optimizeDeps: {
+  //   exclude: ["charmingjs"],
+  // },
+});
