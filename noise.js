89fe6f280b72e2b6a0a64bded62d57f84bce2f89
Bairui Su <subairui@icloud.com>
2025-04-13 15:19:26 -0400

    Symbols (#2)

---
+++ noise.js
@@ -0,0 +1,111 @@
+// Adapting from P5.js
+// @see https://github.com/processing/p5.js/blob/1e6b0caa1e8a8dff3280917d2fd9a84d2e7126ba/src/math/noise.js#L200
+const PERLIN_YWRAPB = 4;
+const PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
+const PERLIN_ZWRAPB = 8;
+const PERLIN_ZWRAP = 1 << PERLIN_ZWRAPB;
+const PERLIN_SIZE = 4095;
+
+function scaledCosine(i) {
+  return 0.5 * (1.0 - Math.cos(i * Math.PI));
+}
+
+function randomLcg(seed) {
+  const m = 4294967296;
+  const a = 1664525;
+  const c = 1013904223;
+  let z = (seed == null ? Math.random() * m : seed) >>> 0;
+  return () => {
+    z = (a * z + c) % m;
+    return z / m;
+  };
+}
+
+function noiseSeed(seed) {
+  const random = randomLcg(seed);
+  const perlin = new Array(PERLIN_SIZE + 1);
+  for (let i = 0; i < PERLIN_SIZE + 1; i++) {
+    perlin[i] = random();
+  }
+  return perlin;
+}
+
+function random(min, max) {
+  [min = 0, max = 1] = arguments.length === 1 ? [0, min] : [min, max];
+  return min + (max - min) * Math.random();
+}
+
+function scaleLinear(domain, range) {
+  return (x) => range[0] + (range[1] - range[0]) * ((x - domain[0]) / (domain[1] - domain[0]));
+}
+
+export function randomNoise(lo = 0, hi = 1, {octaves = 4, seed = random(100000), falloff = 0.5} = {}) {
+  const map = scaleLinear([0, 1], [lo, hi]);
+  const perlin = noiseSeed(seed);
+
+  const noise = (x, y = 0, z = 0) => {
+    if (x < 0) x = -x;
+    if (y < 0) y = -y;
+    if (z < 0) z = -z;
+
+    let xi = Math.floor(x),
+      yi = Math.floor(y),
+      zi = Math.floor(z);
+    let xf = x - xi;
+    let yf = y - yi;
+    let zf = z - zi;
+    let rxf, ryf;
+
+    let r = 0;
+    let ampl = 0.5;
+
+    let n1, n2, n3;
+
+    for (let o = 0; o < octaves; o++) {
+      let of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+
+      rxf = scaledCosine(xf);
+      ryf = scaledCosine(yf);
+
+      n1 = perlin[of & PERLIN_SIZE];
+      n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+      n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+      n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+      n1 += ryf * (n2 - n1);
+
+      of += PERLIN_ZWRAP;
+      n2 = perlin[of & PERLIN_SIZE];
+      n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+      n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+      n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+      n2 += ryf * (n3 - n2);
+
+      n1 += scaledCosine(zf) * (n2 - n1);
+
+      r += n1 * ampl;
+      ampl *= falloff;
+      xi <<= 1;
+      xf *= 2;
+      yi <<= 1;
+      yf *= 2;
+      zi <<= 1;
+      zf *= 2;
+
+      if (xf >= 1.0) {
+        xi++;
+        xf--;
+      }
+      if (yf >= 1.0) {
+        yi++;
+        yf--;
+      }
+      if (zf >= 1.0) {
+        zi++;
+        zf--;
+      }
+    }
+    return r;
+  };
+
+  return (x0 = 0, y0 = 0, z0 = 0) => map(noise(x0, y0, z0));
+}
--- render.js
+++ render.js
@@ -1,103 +1,189 @@
+import {d3, cm} from "./namespaces.js";
 import {interpolate as interpolatePath} from "flubber";
-import {cm, d3} from "./namespaces.js";
 
 const COLORS = [
   d3.interpolateViridis,
   d3.interpolateInferno,
   d3.interpolateMagma,
-  d3.interpolatePlasma,
-  d3.interpolateWarm,
-  d3.interpolateCool,
-  d3.interpolateRainbow,
   d3.interpolateCubehelixDefault,
-  d3.interpolateBlues,
-  d3.interpolateGreens,
-  d3.interpolateGreys,
-  d3.interpolateOranges,
-  d3.interpolatePurples,
-  d3.interpolateReds,
-  d3.interpolateBuGn,
-  d3.interpolateBuPu,
-  d3.interpolateGnBu,
-  d3.interpolateOrRd,
-  d3.interpolatePuBuGn,
-  d3.interpolatePuBu,
-  d3.interpolatePuRd,
-  d3.interpolateRdPu,
-  d3.interpolateYlGnBu,
-  d3.interpolateYlGn,
-  d3.interpolateYlOrBr,
-  d3.interpolateYlOrRd,
+  d3.interpolateBrBG,
+  d3.interpolatePRGn,
+  d3.interpolatePiYG,
+  d3.interpolatePuOr,
+  d3.interpolateRdBu,
 ];
 
 const SYMBOLS = [cm.symbolX, cm.symbolCircle, cm.symbolDiamond, cm.symbolSquare];
 
-export function render(container) {
-  const size = 640;
-  const shapes = [];
-  const random = d3.randomUniform(-25, 25);
-  let [dx0, dy0, dx1, dy1, i0, i1, j0, j1] = [0, 0, 0, 0, 0, 0, 0, 0];
+function fillSquares(rows, randomInt = d3.randomInt) {
+  const cells = [{x: 0, y: 0, x1: rows, y1: rows}];
 
-  drawShape(size / 2, size / 2, 150);
+  fill(0);
 
-  const input = d3.extent(shapes.map((d) => d.r)).reverse();
-  const scale = (i) => d3.scaleSequential(input, COLORS[i]);
-
-  function drawShape(x, y, r) {
-    if (r < 4) return;
-    shapes.push({x, y, r});
-    drawShape(x, y, r - 15);
+  function fill(i) {
+    const cell = cells[i];
+    const point = pick(cell);
+    const subcells = divide(cell, point).filter(defined);
+    cells.splice(i, 1, ...subcells);
+    for (let j = subcells.length - 1; j >= 0; j--) {
+      if (!square(subcells[j])) fill(i + j);
+    }
   }
 
-  function draw({frameCount}) {
-    if (frameCount !== 0) {
-      [dx0, dy0] = [dx1, dy1];
-      [dx1, dy1] = [random(), random()];
-      [i0, i1] = [i1, frameCount === 1 ? 0 : d3.randomInt(COLORS.length)()];
-      [j0, j1] = [j1, frameCount === 1 ? 0 : d3.randomInt(SYMBOLS.length)()];
-    }
+  function divide(cell, point) {
+    const {x, y, x1, y1} = cell;
+    const [px, py] = point;
+    return [
+      {x, y, x1: px, y1: py},
+      {x: px, y, x1: x1, y1: py},
+      {x: px, y: py, x1, y1},
+      {x: x, y: py, x1: px, y1: y1},
+    ];
+  }
 
-    const [c0, c1] = [scale(i0), scale(i1)];
-    const [s0, s1] = [SYMBOLS[j0], SYMBOLS[j1]];
+  function defined({x, y, x1, y1}) {
+    return x1 - x >= 1 && y1 - y >= 1;
+  }
 
-    return cm.svg("g", shapes, {
-      transform: (_, i) => `translate(${i * dx0},${i * dy0})`,
-      transition: (d, i) => [
-        {
-          transform: `translate(${i * dx1},${i * dy1})`,
-          duration: 1000,
-          delay: i * 20,
-          ease: d3.easeElastic,
-        },
-      ],
-      children: (d, i) => {
-        const p = [d.x - d.r, d.y - d.r, d.r * 2];
-        const path = () => interpolatePath(s0(...p), s1(...p));
-        return [
-          cm.svg("path", {
-            stroke: "#000",
-            d: s0(...p),
-            fill: c0(d.r),
-            strokeWidth: 0.5,
-            transition: [
-              {fill: c1(d.r), duration: 500, delay: i * 25},
-              {d: path, duration: 500, delay: 500 + i * 25},
-            ],
-          }),
-        ];
+  // Don't just generate one big blank square!!!
+  function square({x, y, x1, y1}) {
+    return x1 - x === y1 - y && x1 - x !== rows;
+  }
+
+  function pick(cell) {
+    const {x, y, x1, y1} = cell;
+    const points = d3
+      .cross(d3.range(x, x1 + 1), d3.range(y, y1 + 1))
+      .filter((point) => divide(cell, point).some(square));
+    return points[randomInt(points.length)()];
+  }
+
+  return cells;
+}
+
+function createSymbols(cells, {randomInt}) {
+  const randomSeed = randomInt(100000);
+  const randomColorIndex = randomInt(COLORS.length);
+  const randomSymbolIndex = randomInt(SYMBOLS.length);
+  const data = cells
+    .map((d) => {
+      const symbolIndex = randomSymbolIndex();
+      return {
+        x: (d.x + d.x1) / 2,
+        y: (d.y + d.y1) / 2,
+        fromX: 0,
+        fromY: 0,
+        toX: 0,
+        toY: 0,
+        fromColorIndex: 0,
+        toColorIndex: 0,
+        fromSymbolIndex: symbolIndex,
+        toSymbolIndex: symbolIndex,
+        shapes: createShape(0, 0, (d.x1 - d.x) / 2 - 10),
+      };
+    })
+    .sort((a, b) => a.shapes.length - b.shapes.length);
+
+  function createShape(x, y, r) {
+    const shapes = [];
+    const count = Math.sqrt(r * 0.8);
+    const step = r / count;
+    for (let i = 0; i < count; i++) shapes.push({x, y, r: step * i});
+    return shapes.reverse();
+  }
+
+  return {
+    tick({frameCount}) {
+      const randomX = cm.randomNoise(-25, 25, {seed: randomSeed()});
+      const randomY = cm.randomNoise(-25, 25, {seed: randomSeed()});
+      const colorIndex = randomColorIndex();
+      for (const datum of data) {
+        if (frameCount !== 0) {
+          [datum.fromX, datum.toX] = [datum.toX, randomX(datum.x * 0.005, datum.y * 0.005)];
+          [datum.fromY, datum.toY] = [datum.toY, randomY(datum.x * 0.005, datum.y * 0.005)];
+          [datum.fromColorIndex, datum.toColorIndex] = [datum.toColorIndex, colorIndex];
+          [datum.fromSymbolIndex, datum.toSymbolIndex] = [datum.toSymbolIndex, randomSymbolIndex()];
+        }
+      }
+    },
+    data() {
+      return data;
+    },
+  };
+}
+
+function symbol({shapes, fromX, fromY, toX, toY, fromColorIndex, toColorIndex, fromSymbolIndex, toSymbolIndex}) {
+  const R = d3.extent(shapes.map((d) => d.r)).reverse();
+  const color = (i) => d3.scaleSequential(R, COLORS[i]);
+  return cm.svg("g", shapes, {
+    transform: (_, i) => `translate(${i * fromX}, ${i * fromY})`,
+    transition: (_, i) => [
+      {
+        transform: `translate(${i * toX},${i * toY})`,
+        duration: 1000,
+        delay: i * 20,
+        ease: d3.easeElastic,
       },
-    });
+    ],
+    children: (d) => {
+      const p = [d.x - d.r, d.y - d.r, d.r * 2];
+      const path = () => interpolatePath(SYMBOLS[fromSymbolIndex](...p), SYMBOLS[toSymbolIndex](...p));
+      return [
+        cm.svg("path", {
+          d: path(),
+          fill: color(fromColorIndex)(d.r),
+          stroke: "#000",
+          transition: (_, i) => [
+            {
+              fill: color(toColorIndex)(d.r),
+              duration: 500,
+              delay: i * 20,
+            },
+            {d: path, duration: 500, delay: 500 + i * 20},
+          ],
+        }),
+      ];
+    },
+  });
+}
+
+export function render({seed} = {}) {
+  const randomInt = seed === undefined ? d3.randomInt : d3.randomInt.source(d3.randomLcg(seed));
+  const randomUniform = seed === undefined ? d3.randomUniform : d3.randomUniform.source(d3.randomLcg(seed));
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const size = 800;
+  const padding = 10;
+  const rows = 8;
+  const unit = (size - padding * 2) / rows;
+  const pos = (x) => padding + x * unit;
+  const cells = fillSquares(rows, randomInt).map((d) => ({x: pos(d.x), y: pos(d.y), x1: pos(d.x1), y1: pos(d.y1)}));
+  const symbols = createSymbols(cells, {randomInt, randomUniform});
+  const data = symbols.data();
+
+  function draw({frameCount}) {
+    symbols.tick({frameCount});
+    return [
+      cm.svg("g", {
+        transform: `translate(${(width - size) / 2}, ${(height - size) / 2})`,
+        children: [
+          cm.svg("g", data, {
+            transform: (d) => `translate(${d.x}, ${d.y})`,
+            children: (d) => [symbol(d)],
+          }),
+        ],
+      }),
+    ];
   }
 
   const app = cm.app({
-    draw,
     loop: true,
     frameRate: 0.5,
-    width: size,
-    height: size,
+    width,
+    height,
     use: {transition: cm.transition},
+    draw,
   });
 
-  const node = app.render();
-  container.appendChild(node);
+  return app.render();
 }
